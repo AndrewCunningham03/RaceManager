848d8df0d3e7c2755066f532a7fdef9c278aa36e
Andrew <Andrewcunningham9911@hotmail.com>
2024-10-18 17:27:06 +0100

    Racegoer and horse owner complete

--- Program.cs
+++ Program.cs
@@ -6,28 +6,30 @@ namespace RaceManager
     internal class Program
     {
         static List<Event> events = new List<Event>();
-
+        static List<Race> races = new List<Race>();
+        static List<Race> racesTwo = new List<Race>();
+        static List<Race> racesThree = new List<Race>();
 
         static void Main(string[] args)
         {
-            Horse horse1 = new Horse("Wizard of odds", new DateOnly(2022, 1, 24), "WOO2022", "Heaney Group", "Eoin Powell");
-            Horse horse2 = new Horse("Midnight Runner", new DateOnly(2019, 8, 21), "MR2019", "Riley Farms", "Liam O'Connor");
-            Horse horse3 = new Horse("Blazing Fury", new DateOnly(2021, 6, 30), "BF2021", "McCarthy Racing", "Paul McGrath");
-            Horse horse4 = new Horse("Silver Bullet", new DateOnly(2018, 2, 18), "SB2018", "Murphy Bros", "Declan Finn");
-            Horse horse5 = new Horse("Eagle Eye", new DateOnly(2022, 3, 25), "EE2022", "Kennedy Farms", "Sean Donnelly");
-            Horse horse6 = new Horse("Golden Arrow", new DateOnly(2019, 4, 14), "GA2019", "Murphy Bros", "Kevin Daly");
-            Horse horse7 = new Horse("Thunderstrike", new DateOnly(2020, 11, 5), "TS2020", "Collins Racing", "Ryan Kelly");
-            Horse horse8 = new Horse("Wildfire", new DateOnly(2021, 7, 9), "WF2021", "Flanagan Stables", "Patrick O'Reilly");
-            Horse horse9 = new Horse("Shadow Dancer", new DateOnly(2020, 9, 15), "SD2020", "Doyle Equestrian", "Conor Byrne");
-            Horse horse10 = new Horse("Highlander", new DateOnly(2017, 12, 1), "HL2017", "Gallagher Farms", "Brian Mahon");
-            Horse horse11 = new Horse("Lightning Bolt", new DateOnly(2021, 5, 27), "LB2021", "Farrell Racing", "Aidan Quinn");
-            Horse horse12 = new Horse("Firestorm", new DateOnly(2019, 10, 30), "FS2019", "O'Shea Stables", "Cian Keane");
-            Horse horse13 = new Horse("Star Voyager", new DateOnly(2020, 3, 19), "SV2020", "Ryan Stables", "Brendan Murphy");
-            Horse horse14 = new Horse("Ocean Breeze", new DateOnly(2021, 8, 11), "OB2021", "Collins Racing", "Fionn Brady");
-            Horse horse15 = new Horse("Moonlight Shadow", new DateOnly(2018, 11, 2), "MS2018", "Nolan Stables", "James Hughes");
-            Horse horse16 = new Horse("Delta Work", new DateOnly(2016, 12, 24), "DW2016", "Heaney Group", "Micheal Blaney");
-            Horse horse17 = new Horse("Mighty Potter", new DateOnly(2020, 1, 20), "MP2020", "Collins Racing", "Jack Collins");
-            Horse horse18 = new Horse("Sir Bob", new DateOnly(2017, 5, 2), "SB2017", "Murphy Bros", "Gareth Murphy");
+            Horse horse1 = new Horse("Wizard of odds", 4, "WOO2022", "Heaney Group", "Eoin Powell");
+            Horse horse2 = new Horse("Midnight Runner", 7, "MR2019", "Riley Farms", "Liam O'Connor");
+            Horse horse3 = new Horse("Blazing Fury", 5, "BF2021", "McCarthy Racing", "Paul McGrath");
+            Horse horse4 = new Horse("Silver Bullet", 5, "SB2018", "Murphy Bros", "Declan Finn");
+            Horse horse5 = new Horse("Eagle Eye", 3, "EE2022", "Kennedy Farms", "Sean Donnelly");
+            Horse horse6 = new Horse("Golden Arrow", 5, "GA2019", "Murphy Bros", "Kevin Daly");
+            Horse horse7 = new Horse("Thunderstrike", 7, "TS2020", "Collins Racing", "Ryan Kelly");
+            Horse horse8 = new Horse("Wildfire", 9, "WF2021", "Flanagan Stables", "Patrick O'Reilly");
+            Horse horse9 = new Horse("Shadow Dancer", 6, "SD2020", "Doyle Equestrian", "Conor Byrne");
+            Horse horse10 = new Horse("Highlander", 7, "HL2017", "Gallagher Farms", "Brian Mahon");
+            Horse horse11 = new Horse("Lightning Bolt", 5, "LB2021", "Farrell Racing", "Aidan Quinn");
+            Horse horse12 = new Horse("Firestorm", 3, "FS2019", "O'Shea Stables", "Cian Keane");
+            Horse horse13 = new Horse("Star Voyager", 10, "SV2020", "Ryan Stables", "Brendan Murphy");
+            Horse horse14 = new Horse("Ocean Breeze", 8, "OB2021", "Collins Racing", "Fionn Brady");
+            Horse horse15 = new Horse("Moonlight Shadow", 5, "MS2018", "Nolan Stables", "James Hughes");
+            Horse horse16 = new Horse("Delta Work", 9, "DW2016", "Heaney Group", "Micheal Blaney");
+            Horse horse17 = new Horse("Mighty Potter", 6, "MP2020", "Collins Racing", "Jack Collins");
+            Horse horse18 = new Horse("Sir Bob", 8, "SB2017", "Murphy Bros", "Gareth Murphy");
 
             List<Horse> horseListOne = new List<Horse>();
             horseListOne.Add(horse1);
@@ -59,20 +61,20 @@ namespace RaceManager
             horseListSix.Add(horse17);
             horseListSix.Add(horse18);
 
-            Race r1 = new Race("race 1", new DateTime(2024, 3, 25, 12, 20, 0), horseListOne);
-            Race r2 = new Race("Race 2", new DateTime(2024, 3, 25, 13, 0, 0), horseListTwo);
-            Race r3 = new Race("Race 3", new DateTime(2024, 5, 25, 13, 30, 0), horseListThree);
-            Race r4 = new Race("Race 4", new DateTime(2024, 5, 25, 14, 0, 0), horseListFour);
-            Race r5 = new Race("Race 5", new DateTime(2024, 10, 25, 14, 45, 0), horseListFive);
-            Race r6 = new Race("Race 6", new DateTime(2024, 10, 25, 15, 30, 0), horseListSix);
+            Race r1 = new Race("race 1", new DateTime(2024, 3, 25, 12, 20, 0),horseListOne);
+            Race r2 = new Race("Race 2", new DateTime(2024, 3, 25, 13, 0, 0),horseListTwo);
+            Race r3 = new Race("Race 3", new DateTime(2024, 5, 25, 13, 30, 0),horseListThree);
+            Race r4 = new Race("Race 4", new DateTime(2024, 5, 25, 14, 0, 0),horseListFour);
+            Race r5 = new Race("Race 5", new DateTime(2024, 10, 25, 14, 45, 0),horseListFive);
+            Race r6 = new Race("Race 6", new DateTime(2024, 10, 25, 15, 30, 0),horseListSix);
 
-            List<Race> races = new List<Race>();
+
             races.Add(r1);
             races.Add(r2);
-            List<Race> racesTwo = new List<Race>();
+
             racesTwo.Add(r3);
             racesTwo.Add(r4);
-            List<Race> racesThree = new List<Race>();
+
             racesThree.Add(r5);
             racesThree.Add(r6);
 
@@ -84,47 +86,203 @@ namespace RaceManager
             events.Add(event2);
             events.Add(event3);
 
-            bool showMenu = true;
-            while (showMenu)
+
+
+
+            int num = 0;
+            while (num != 4)
             {
-                showMenu = MainMenu();
+                String[] array = new string[4];
+
+
+                array[0] = "1. Racegoer";
+                array[1] = "2. Horse Owner";
+                array[2] = "3. Racecourse/Event Manager";
+                array[3] = "4. Exit";
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+                Console.WriteLine("Enter a number");
+                String intput = Console.ReadLine();
+
+                num = int.Parse(intput);
+
+                if (num <= 0 || num > array.Length)
+                {
+                    Console.WriteLine("Number is not on menu");
+                }
+
+                {
+                    switch (num)
+                    {
+                        case 1:
+                            RacegoerMenu();
+                            break;
+                        case 2:
+                            HorseOwnerMenu();
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option, please try again.");
+                            break;
+                    }
+                }
+
+
+
+                static void RacegoerMenu()
+                {
+                    int num = 0;
+                    while (num != 3)
+                    {
+                        String[] array = new String[3];
+
+                        array[0] = "1. View all events";
+                        array[1] = "2. Enter name of Event to view Races and the horses";
+                        array[2] = "3. Exit";
+
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            Console.WriteLine(array[i]);
+                        }
+                        Console.WriteLine("Enter a number");
+                        String intput = Console.ReadLine();
+
+                        num = int.Parse(intput);
+
+                        if (num <= 0 || num > array.Length)
+                        {
+                            Console.WriteLine("Number is not on menu");
+                        }
+
+
+                        switch (num)
+                        {
+                            case 1:
+                                AllEvents();
+                                break;
+                            case 2:
+                                AllEvents();
+                                Console.WriteLine("Enter event name of races you want to view");
+                                String eventName = Console.ReadLine();
+                                Event selectedEvent = events.FirstOrDefault(x => x.Name == eventName);
+                                if (selectedEvent == null)
+                                {
+                                    Console.WriteLine("Event not found.Please re-enter");
+                                    return;
+                                }
+                                Console.WriteLine($"Races for the event: {selectedEvent.Name}");
+                                if (selectedEvent.NumberOfRaces == 0)
+                                {
+                                    Console.WriteLine("No races available for this event.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Races in this event:");
+                                    foreach (Race race in selectedEvent.Racelist)
+                                    {
+                                        Console.WriteLine(race.ToString());
+
+                                        if (race.HorseList.Count == 0)
+                                        {
+                                            Console.WriteLine("No horses are entered in this race.");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Horses in this race:");
+                                            foreach (Horse horse in race.HorseList)
+                                            {
+                                                Console.WriteLine(horse.ToString());
+                                            }
+                                        }
+                                    }
+                                }
+                                break;
+                            case 3:
+                                Console.WriteLine("Signing out...");
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+                }
+                    //display all events
+                    static void AllEvents()
+                    {
+                        if (events.Count == 0)
+                        {
+                            Console.WriteLine("There are no events available at this time");
+                        }
+                        for (int i = 0; i < events.Count; i++)
+                        {
+                            Console.WriteLine(events[i].ToString());
+                        }
+                    }
+                    static void HorseOwnerMenu()
+                    {
+                        AllEvents();
+                        Console.WriteLine("Enter name of event you would like to your horse to");
+                        String eventName = Console.ReadLine();
+                        Event selectedEvent = events.FirstOrDefault(x => x.Name == eventName);
+                        if (selectedEvent == null)
+                        {
+                            Console.WriteLine("Event not found.Please re-enter");
+                            return;
+                        }
+                        Console.WriteLine($"Races for the event: {selectedEvent.Name}");
+                        if (selectedEvent.NumberOfRaces == 0)
+                        {
+                            Console.WriteLine("No races available for this event.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Races in this event:");
+                            foreach (Race race in selectedEvent.Racelist)
+                            {
+                                Console.WriteLine(race.ToString());
+                            }
+                        }
+                        Console.WriteLine("Enter name of race you want to enter horse into");
+                        String raceName = Console.ReadLine();
+
+                        Race selectRace = selectedEvent.Racelist.FirstOrDefault(r => r.RaceName == raceName);
+
+                        if (selectRace == null)
+                        {
+                            Console.WriteLine("Race not found.Please re-enter");
+                            return;
+                        }
+                        Console.WriteLine("Enter horse name");
+                        String horseName = Console.ReadLine();
+                        Console.WriteLine("Enter horse age");
+                        String horseAgeString = Console.ReadLine();
+                        int horseAge = int.Parse(horseAgeString);
+                        Console.WriteLine("Enter horse ID");
+                        String horseID = Console.ReadLine();
+                        Console.WriteLine("Enter owners name");
+                        String ownersName = Console.ReadLine();
+                        Console.WriteLine("Enter jockey name");
+                        String jockey = Console.ReadLine();
+
+                        Horse horse = new Horse(horseName, horseAge, horseID, ownersName, jockey);
+
+                        selectRace.AddHorse(horse);
+
+                        Console.WriteLine($"Horse {horse.Name} has been added to {selectRace.RaceName}, in {selectedEvent.Name}");
+                    }
+
+
+
+
+
+
+                }
             }
         }
-        static void RacegoerMenu()
-        {
-            if (events.Count == 0)
-            {
-                Console.WriteLine("No events are available at this time.");
-                return;
-            }
-            
-        }
-
-        private static bool MainMenu()
-        {
-            Console.Clear();
-            Console.WriteLine("1. Racegoer");
-            Console.WriteLine("2. Horse Owner");
-            Console.WriteLine("3. Racecourse/Event Manager");
-            Console.WriteLine("4. Exit");
-            Console.Write("\r\nSelect an option: ");
-
-            switch (Console.ReadLine())
-            {
-                case "1":
-                    RacegoerMenu();
-                    return true;
-                case "2":
-                    return true;
-                case "3":
-                    return true;
-                case "4":
-                    return false;
-                default:
-                    return true;
-            }
-        }
-
-            
     }
-}
