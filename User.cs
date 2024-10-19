034e3110d293f399340c5dd140008a0786f4e6bd
Andrew <Andrewcunningham9911@hotmail.com>
2024-10-19 16:02:55 +0100

    User added

---
+++ User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceManager
+{
+    internal class User
+    {
+        public enum UserType
+        {
+            Racegoer,
+            HorseOwner,
+            RaceManager
+        }
+        private String email;
+        private String password;
+        private UserType type;
+
+        public User(string email, string password, UserType type)
+        {
+            this.email = email;
+            this.password = password;
+            this.type = type;
+        }
+        public override String ToString()
+        {
+            return $"Email:{email}, Password:{password}, Type:{type}";
+        }
+    }
+}
